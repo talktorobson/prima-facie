e6311659337c2cf23ada7e493b4f4f6d6722f752
Robson Benevenuto D'Avila Reis <talktorobson@gmail.com>
2026-02-07 19:40:30 +0100

    feat: wire DashboardHeader into dashboard layout
    Adds search bar and notification bell to the top of all dashboard
    pages on desktop viewports.

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,6 +2,7 @@
 
 import { Sidebar } from '@/components/layout/sidebar'
 import { MobileMenu } from '@/components/layout/mobile-menu'
+import { DashboardHeader } from '@/components/layout/dashboard-header'
 import { useAuthContext } from '@/lib/providers/auth-provider'
 import { LoadingSpinner } from '@/components/ui/loading-spinner'
 import { FirmContextGuard } from '@/components/platform/firm-context-guard'
@@ -27,6 +28,9 @@ export default function DashboardLayout({
       <MobileMenu />
 
       <main className="lg:pl-64">
+        <div className="hidden lg:block">
+          <DashboardHeader />
+        </div>
         <div className="px-4 sm:px-6 lg:px-8 py-8 lg:py-8 pt-20 lg:pt-8">
           <FirmContextGuard>
             {children}
